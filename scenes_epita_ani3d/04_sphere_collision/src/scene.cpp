50ab6829b79db3d67cb8d4146e21064a948cecea
Arthur Le Bourg <lebourgarthur@gmail.com>
2022-10-16 16:01:35 +0200

    small opti

--- scenes_epita_ani3d/04_sphere_collision/src/scene.cpp
+++ scenes_epita_ani3d/04_sphere_collision/src/scene.cpp
@@ -87,12 +87,12 @@ void scene_structure::display_frame()
 	{
 		particles->add_boule(i, walls, dt, particles);
 	}
-	grid_3D<float> field = compute_scalar_field(domain, *particles, sigma);
 
 	// Compute the mesh using marching cube
 	
 	if (gui.marching_cube)
 	{
+		grid_3D<float> field = compute_scalar_field(domain, *particles, sigma);
 		mesh m = marching_cube(field, domain, isovalue);
 		implicit_surface.initialize_data_on_gpu(m);
     	implicit_surface.material.color = {0.83/2,0.94/2,0.97};
